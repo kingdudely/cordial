ed2663baaef8f952c2264cc55995b9700a935fb9
a <102840087+kingdudely@users.noreply.github.com>
2026-09-24 17:42:39 -0400

    Remove unused libbadcpu source third_party/libbadcpu/src/badcpu.cpp

--- third_party/libbadcpu/src/badcpu.cpp
+++
@@ -1,8 +0,0 @@
-#include "badcpu.h"
-
-namespace badcpu {
-
-// Library entry point is handled by the constructor attribute in signal_handler.cpp
-// This file exists to ensure the library can be linked correctly.
-
-} // namespace badcpu
